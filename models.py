89574e898d44c45cab4de1d805b5e4cc7ef1102f
ms.brown <eqbrown@isf.edu.hk>
2026-04-29 11:05:16 +0800

    setup for student usage - deleted most files and moved to branch

--- models.py
+++ models.py
@@ -4,14 +4,3 @@ db = SQLAlchemy()
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
-    science1 = db.Column(db.String(120), unique=False, nullable=False)
-    science2 = db.Column(db.String(120), unique=False, nullable=True)
-    science3 = db.Column(db.String(120), unique=False, nullable=True)
-
-    group_num = db.Column(db.Integer, unique=False, nullable=False, default=0)
-
-
-    def __repr__(self):
-        return f"{self.name} ({self.science1}, {self.science2}, {self.science3}) (Group {self.group_num})" 
-    
