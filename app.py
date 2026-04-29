e97f792e0316b46d2b587ebc42e70c5e4f630f3a
ms.brown <eqbrown@isf.edu.hk>
2026-04-29 11:45:00 +0800

    update for students

--- app.py
+++ app.py
@@ -3,9 +3,8 @@
 
 from flask import Flask, request, render_template, url_for, redirect, request
 from sqlalchemy import text
-from models import db, Student  # Import from your other file
+from models import db, Student  
 import secrets
-
 
 app = Flask(__name__, static_url_path=f'/')
 app.secret_key = secrets.token_hex(32)  # Required for CSRF protection
@@ -17,6 +16,7 @@ db.init_app(app)
 # Create the tables if it does not exisit, ignore if it exists 
 with app.app_context():
     db.create_all()
+
 
 
 if __name__ == '__main__': 
